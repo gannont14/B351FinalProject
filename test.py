f371006f4b26f67a19244f056e7e47deb0b806f1
gannont14 <tubbsgannon14@gmail.com>
2024-11-04 10:59:56 -0500

    updated guess to have min heap to track best score, added check if game is over

--- test.py
+++ test.py
@@ -1,18 +1,2 @@
-import time 
+print("Hello World")
 
-from selenium import webdriver
-
-driver = webdriver.Firefox()
-print(type(webdriver.Firefox())
-
-# Contexo url : https://contexto.me/
-driver.get("https://contexto.me/")
-
-
-print(driver.title)
-
-# time.sleep(15)
-
-end = input("enter:")
-if(end == '1'):
-    driver.quit()
