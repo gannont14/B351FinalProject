d89e46a6a446a6bf142d2e9abab789efb4095463
Jason Ballinger <jasonballinger_dev@yahoo.com>
2024-11-29 00:35:45 -0600

    dataset

--- sklearn-track/main.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-import nltk
-
-text = ["The quick brown fox jumped over the lazy dog"]
-
-vectorizer = CountVectorizer()
-vectorizer.fit(text)
-print(vectorizer.vocabulary_)
